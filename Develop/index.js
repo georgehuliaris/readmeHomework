67f14e3a8aeaf11371444ac3c32efff688ef92c7
George Huliaris <georgehuliaris@me.com>
2021-03-30 15:54:04 -0500

    initial commit

---
+++ Develop/index.js
@@ -0,0 +1,45 @@
+// TODO: Include packages needed for this application/ done!
+const inquirer = require('inquirer');
+const fs = require('fs');
+// TODO: Create an array of questions for user input/ still need to put content
+// const questions = [];
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'checkbox',
+      message: 'What languages do you know?',
+      name: 'stack',
+      choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+    },
+    {
+      type: 'list',
+      message: 'What is your preferred method of communication?',
+      name: 'contact',
+      choices: ['email', 'phone', 'telekinesis'],
+    },
+  ])
+  .then((data) => {
+    const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+  });
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+
+
